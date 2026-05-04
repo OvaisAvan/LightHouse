426d4787eb8b7381203f5387b2de68192b735315
Owais <ovaisavan1996@gmail.com>
2026-05-04 15:07:15 +0500

    texture assets

---
+++ Assets/Game/BarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarController : MonoBehaviour
+{
+    [SerializeField] private Image _colorIntensityBar;
+    private void OnEnable()
+    {
+        TowerManager.OnColorIntensityUpdate += OnColorIntensityUpdateEvent;
+    }
+    private void OnColorIntensityUpdateEvent(float colorIntensity, float maxColorIntensity)
+    {
+        _colorIntensityBar.fillAmount = colorIntensity/maxColorIntensity;
+    }
+    private void OnDisable()
+    {
+        TowerManager.OnColorIntensityUpdate -= OnColorIntensityUpdateEvent;
+    }
+}
